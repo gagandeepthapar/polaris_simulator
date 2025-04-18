7df3278f77cad3ce7ffe607d2ba7d9a3c9601d28
Gagandeep Thapar <gsthapar@gmail.com>
2025-04-18 15:11:02 -0700

    updating main script with log messages and clean up of initialization, global params

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,66 @@
+pub mod shims;
+
+use altai_rs::quatlib::qangle;
 use polaris_fsw as fsw;
 use polaris_fsw::actuators::types::ActuatorBus;
-use polaris_log::{self as log, types::LogLevel};
 use polaris_plant::{self as plant};
 
-fn main() {
-    // Setup
-    // // Get Logger
-    let (fsw_logger, plant_logger, sim_logger) =
-        log::create_loggers("FSW", 10, "PLANT", 10, "SIM", Some(LogLevel::WARN));
+use serde_yaml;
+use std::{f64::consts::PI, fs, time};
 
+use env_logger;
+use log;
+
+const CONFIG: &str = "./configs/default.yaml";
+const SIM_FIN: f64 = 100.;
+const SC_Ts: f64 = 0.1;
+const Ts: f64 = 0.1;
+const SMALL: f64 = 1e-12;
+
+fn main() {
     // Initialize SIM
-    let mut gnc_sim = sim_logger;
+    env_logger::init();
+
+    // Read YAML Inputs
+    let yaml_str = fs::read_to_string(CONFIG).unwrap();
+    let data: shims::fsw_shim::FSW_ParamShim = serde_yaml::from_str(&yaml_str).unwrap();
+    log::trace!("FSW: {:?}", data);
+
+    let data2: shims::sc_shim::Spacecraft_ParamShim = serde_yaml::from_str(&yaml_str).unwrap();
+    log::trace!("PLANT: {:?}", data2);
 
     // Initialize GNC FCSW
-    println!("Initializing FSW");
-    let mut gnc_fcsw = fsw::FlightSoftware::initialize(fsw_logger);
+    let mut gnc_fcsw = fsw::FlightSoftware::initialize(data.to_fsw());
 
     // Initialize GNC Plant
-    println!("Initializing PLANT");
-    let mut gnc_plant = plant::Spacecraft::initialize(plant_logger);
+    let mut gnc_plant = plant::Spacecraft::initialize(SC_Ts, data2.to_spacecraft());
 
     // Allocate stack data for raw sensor/actuator commands
     let mut raw_sensor_bus = gnc_plant.initial_state();
-    let mut actuator_bus: ActuatorBus;
+    let mut actuator_bus: ActuatorBus = ActuatorBus::default();
 
     // Loop
-    loop {
+    let start = time::Instant::now();
+    for _ in 0..((SIM_FIN / SC_Ts) as usize) {
         // Run GNC
-        println!("READING RAW");
-        actuator_bus = gnc_fcsw.gnc_loop(&mut raw_sensor_bus);
+        if gnc_plant.sim_time % Ts <= SMALL {
+            // Check if running at FSW time
+            actuator_bus = gnc_fcsw.gnc_loop(&mut raw_sensor_bus);
+        }
 
-        // Run Plant
-        println!("COMMANDING ACTUATORS");
+        // Run Plant (Runs at each step)
         raw_sensor_bus = gnc_plant.simulate_plant(&mut actuator_bus);
 
         // Log
     }
+    let end = time::Instant::now();
+
+    // Post Process
+    let dur = end.duration_since(start).as_nanos() as f64 / 1000.;
+    log::error!(
+        "{} sec simulation took {} usec to run; {:.4}x FTRT",
+        SIM_FIN,
+        dur,
+        SIM_FIN * 1e6 / dur
+    );
 }
